4a61e0c957448a64c10e02f1524075f1ecbdab64
gabichagassantos@hotmail.com <gustavosilvah37@gmail.com>
2024-06-15 15:27:27 -0300

    feat: application version changed

---
+++ src/main/java/br/edu/senaisp/MedApi_3/MedApi3Application.java
@@ -0,0 +1,13 @@
+package br.edu.senaisp.MedApi_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedApi3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedApi3Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/senaisp/MedApi_3/MedApi3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.senaisp.MedApi_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedApi3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
