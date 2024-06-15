3356c2718d483abef7c3fcee4b54143a22372999
gabichagassantos@hotmail.com <gustavosilvah37@gmail.com>
2024-06-15 15:41:16 -0300

    fix: fixing my error

--- src/main/java/br/edu/senaisp/MedApi_3/repository/MedApiRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.senaisp.MedApi_3.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.edu.senaisp.MedApi_3.model.Medico;
-
-public interface MedApiRepository extends JpaRepository<Medico, Integer>{
-
-}
